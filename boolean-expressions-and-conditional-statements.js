a318c4e6c7790f56f0e0c1891dfad92bbe3da47f
Shobana Christy <shobichristy07@gmail.com>
2025-07-15 00:06:54 -0500

    Created if conditions and used operators

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -26,11 +26,60 @@ Paste the following code into your editor:
 
 const readline = require('readline-sync');
 
+console.log("You see two paths: one leads to the mountains, the other to the village.");
+const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+const mapChoice = readline.question("Do you have Map (yes/no)?");
+const compassChoice = readline.question("Do you have Compass (yes/no)?");
+const boatChoice = readline.question("Do you have Boat and paddle (yes/no)?");
+
+const hasTorch = true;
+const hasMap = mapChoice.toLowerCase() == "yes" || mapChoice.toLocaleUpperCase() == "yeah"; // used || operator
+const hasCompass = compassChoice.toLowerCase() == "yes" || compassChoice.toLocaleUpperCase() == "yeah";
+const hasBoat = boatChoice.toLowerCase() == "yes" || boatChoice.toLocaleUpperCase() == "yeah";
+const hasPaddle = hasBoat;
+
+let reachedDestination = false;
+if(choice === "village") {
+  if(hasMap && hasCompass && hasTorch) { // used && operator
+    console.log("Now you have map, compass and torch, you can navigate to the village.")
+    if(hasBoat && hasPaddle) { // nested if
+      reachedDestination = true;
+      console.log("You can use the boat and paddle to cross the river.");
+    }
+  } else {
+    console.log("Map and/or compass are missing.So, it's difficult to navigate to village.")
+  }
+} else {
+  if(hasMap && hasCompass && hasTorch) {
+     reachedDestination = true;
+    console.log("Now you have map, compass and torch, you can navigate to the mountain.")
+  }
+}
+
+if(!reachedDestination) {// used ! operator
+  console.log("You have not reached the "+choice);
+} else {
+  console.log("You have reached the "+choice);
+}
+
+
+/* 
+
+Add Customization and expand the game:
+  - Add more choices and scenarios.
+  - Include additional items (e.g., a sword, a compass).
+  - Use nested conditionals and logical operators to create complex outcomes.
+
+*/
+/*const readline = require('readline-sync');
+
 const hasTorch = true;
 const hasMap = false;
 
+
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+
 
 if (choice === "mountains" && hasTorch) {
   console.log("You safely navigate through the dark mountains.");
@@ -41,12 +90,5 @@ if (choice === "mountains" && hasTorch) {
 } else {
   console.log("You get lost and wander aimlessly.");
 }
-
-/* 
-
-Add Customization and expand the game:
-  - Add more choices and scenarios.
-  - Include additional items (e.g., a sword, a compass).
-  - Use nested conditionals and logical operators to create complex outcomes.
 
 */
